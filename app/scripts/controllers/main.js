4a9db63957a792b35f7217088c7dd61446e1d559
Samuel Heinzmann <samuel.heinzmann@swisscom.com>
2016-03-17 10:47:26 +0100

    Generated remoker application with yoeman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name remoker.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of remoker
+ */
+angular.module('remoker')
+  .controller('MainCtrl', function () {
+
+  });
