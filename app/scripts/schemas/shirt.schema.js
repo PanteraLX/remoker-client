b712d003e814fbafa5a8afb421cf8bfedba9b2bb
Samuel Heinzmann <samuel.heinzmann@swisscom.com>
2016-03-18 14:47:42 +0100

    Added service for all schemase

---
+++ app/scripts/schemas/shirt.schema.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name remoker.shirt
+ * @description
+ * # shirt
+ * Constant in remoker
+ */
+angular.module('remoker')
+  .constant('shirt', [
+      {value: 'S', index: 0},
+      {value: 'M', index: 1},
+      {value: 'L', index: 2},
+      {value: 'XL', index: 3}
+  ]);
--- app/scripts/services/schema.service.js
+++ app/scripts/services/schema.service.js
@@ -8,8 +8,36 @@
  * Service in remoker
  */
 angular.module('remoker')
-    .service('schema', function() {
+    .service('schema', function(fibonacci, shirt, cup) {
+
         this.getSchemas = function() {
             return ['fibonacci', 'shirt', 'cup']
-        }
+        };
+
+        this.getCardset = function(sizingStrategy) {
+            var cardSet = [];
+            var rowArray = [];
+            var row = 0;
+            angular.forEach(this.getArray(sizingStrategy), function(value, key) {
+                rowArray.push(value);
+                cardSet[row] = rowArray;
+                if(key % 3 === 2) {
+                    rowArray = [];
+                    row++
+                }
+            });
+            return cardSet;
+        };
+
+        this.getArray = function(sizingStrategy) {
+            switch(sizingStrategy) {
+                case 'shirt':
+                    return shirt;
+                case 'cup':
+                    return cup;
+                default:
+                    return fibonacci;
+            }
+        };
+
     });
